ff88dc4abe12151e50af4e31264c493790869f88
Akarjya <140883060+Akarjya@users.noreply.github.com>
2025-08-16 21:40:48 +0530

    Update WhatismyIP

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ Compress(app)
 resource_cache = {}
 
 # Final website URL (iplocation.com for IP+location)
-FINAL_URL = 'https://iplocation.com/'
+FINAL_URL = 'https://www.whatismyip.com/'
 
 # Spoofed Timezone and Offset for New York
 SPOOFED_TIMEZONE = 'America/New_York'
